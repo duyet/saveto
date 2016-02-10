1cc36414b8afb41ff2e615e5ae46523076da807c
Van-Duyet Le <lvduit08@gmail.com>
2016-02-10 23:17:13 +0700

    Update router, api nested

--- router.js
+++ router.js
@@ -1,12 +1,18 @@
-var router = require('koa-router')();
+var Router = require('koa-router');
+
+var router = new Router();
+var api = new Router();
+
+// Api endpoint
+api
+	.get('/', function *(next) { this.body = { message: '/' }; })
+	.get('/ping', function *(next) { this.body = { message: 'pong' }; })
+	.all('*', function *(next) { this.body = { message: 'Hi, bye!' }; })
 
 router
+	.use('/api/v1', api.routes(), api.allowedMethods())
 	.get('/', function *(next) {
 		this.body = "Hihi";
 	})
-	.get('/api/*', function *(next) {
-		this.body = { message: 'Hi, bye!' };
-	})
-
-
+	
 module.exports = router;
