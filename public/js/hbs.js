005aeeec20d09a7a755617b56bfc58ee5674de00
Van-Duyet Le <lvduit08@gmail.com>
2016-03-07 16:13:54 +0700

    link icon

--- public/js/hbs.js
+++ public/js/hbs.js
@@ -49,3 +49,11 @@ Handlebars.registerHelper('icon_markdown', function(class_css) {
     class_css = (' class="' +class_css + '"') || '';
     return new Handlebars.SafeString('<svg aria-hidden="true" '+ class_css + ' height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16"><path d="M14.85 3H1.15C0.52 3 0 3.52 0 4.15v7.69C0 12.48 0.52 13 1.15 13h13.69c0.64 0 1.15-0.52 1.15-1.15V4.15C16 3.52 15.48 3 14.85 3zM9 11L7 11V8l-1.5 1.92L4 8v3H2V5h2l1.5 2 1.5-2 2 0V11zM11.99 11.5L9.5 8h1.5V5h2v3h1.5L11.99 11.5z"></path></svg></span>');
 })
+
+Handlebars.registerHelper('equal', function(var1, var2, context) {
+    if(var1 == var2) {
+        return context.fn(this);
+    }
+    
+    return context.inverse(this);
+});
