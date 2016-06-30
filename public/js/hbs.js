143fdf49dfbe43044a98693a5122210f1403f304
Van-Duyet Le <lvduit08@gmail.com>
2016-06-30 17:34:55 +0700

    Image proxy and handlebar helpers resize image

--- public/js/hbs.js
+++ public/js/hbs.js
@@ -49,6 +49,34 @@ Handlebars.registerHelper('shorten_url', function(alias, block) {
     return new Handlebars.SafeString(result);
 });
 
+Handlebars.registerHelper('resize_w', function(resize_w, url) {
+    if (typeof url == 'object') { // not defined
+        url = resize_w;
+        resize_w = 300; // default
+    }
+
+    var result = '//images1-focus-opensocial.googleusercontent.com/gadgets/proxy?resize_w=' 
+        + resize_w +'&container=focus&url=' + encodeURIComponent(url);
+    return new Handlebars.SafeString(result);
+});
+
+Handlebars.registerHelper('resize_h', function(resize_h, url) {
+    if (typeof url == 'object') { // not defined
+        url = resize_h;
+        resize_h = 300; // default
+    }
+
+    var result = '//images1-focus-opensocial.googleusercontent.com/gadgets/proxy?resize_h='
+        + resize_h +'&container=focus&url=' + encodeURIComponent(url);
+    return new Handlebars.SafeString(result);
+});
+
+Handlebars.registerHelper('img_proxy', function(url) {
+    var result = '//images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&url=' 
+        + encodeURIComponent(url);
+    return new Handlebars.SafeString(result);
+});
+
 Handlebars.registerHelper('icon_markdown', function(class_css) {
     class_css = (' class="' +class_css + '"') || '';
     return new Handlebars.SafeString('<svg aria-hidden="true" '+ class_css + ' height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16"><path d="M14.85 3H1.15C0.52 3 0 3.52 0 4.15v7.69C0 12.48 0.52 13 1.15 13h13.69c0.64 0 1.15-0.52 1.15-1.15V4.15C16 3.52 15.48 3 14.85 3zM9 11L7 11V8l-1.5 1.92L4 8v3H2V5h2l1.5 2 1.5-2 2 0V11zM11.99 11.5L9.5 8h1.5V5h2v3h1.5L11.99 11.5z"></path></svg></span>');
