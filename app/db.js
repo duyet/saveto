fa69a9b4cc3428a4443c6145b242ea11adf146cd
Van-Duyet Le <lvduit08@gmail.com>
2016-07-22 10:36:11 +0700

    Update JSDoc

--- app/db.js
+++ app/db.js
@@ -1,7 +1,16 @@
+/**
+ * Database module
+ * @module DB
+ */
+
 var mongoose = require('mongoose');
 var config = require('./config');
 
 mongoose.connect(config.db);
+
+/**
+ * Set DB Debug mode
+ */
 mongoose.set('debug', config.debug);
 
 module.exports = mongoose;
